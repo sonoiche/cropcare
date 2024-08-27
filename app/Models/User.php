f2e31d31bfb57790f2b60b7a171e86939e5fe96b
Jelson Lanto <jelsonlanto@Jelsons-iMac.local>
2024-08-27 13:42:31 +0800

    update files to the latest changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable
     protected $guarded = [];
 
     CONST ROLE_AGRICULTURIST = "Agriculturist";
+    CONST ROLE_PRESIDENT = "ROLE_PRESIDENT";
 
     /**
      * The attributes that should be hidden for serialization.
